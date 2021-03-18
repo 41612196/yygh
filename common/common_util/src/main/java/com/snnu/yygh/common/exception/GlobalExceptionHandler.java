90d7c48584787c486fd02e878908c46b23f4150d
41612196 <1185391863@qq.com>
2021-03-18 23:16:04 +0800

    后端医院设置

---
+++ common/common_util/src/main/java/com/snnu/yygh/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.snnu.yygh.common.exception;
+
+import com.snnu.yygh.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+    @ExceptionHandler(YyghException.class)
+    @ResponseBody
+    public Result error(YyghException e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+}
--- service/service_hosp/src/main/java/com/snnu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/snnu/yygh/hosp/controller/HospitalSetController.java
@@ -1,15 +1,21 @@
 package com.snnu.yygh.hosp.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.snnu.yygh.common.result.Result;
+import com.snnu.yygh.common.util.MD5;
 import com.snnu.yygh.hosp.service.HospitalSetService;
 import com.snnu.yygh.model.hosp.HospitalSet;
+import com.snnu.yygh.vo.hosp.HospitalSetQueryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 @Api(tags = "医院设置管理")
 @RestController
@@ -34,7 +40,86 @@ public class HospitalSetController {
         } else {
             return Result.fail();
         }
+    }
+
+    @PostMapping("findPageHospSet/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        Page<HospitalSet> page = new Page<>(current, limit);
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+        if (!StringUtils.isEmpty(hosname)) {
+            wrapper.like("hosname", hospitalSetQueryVo.getHosname());
+        }
+        if (!StringUtils.isEmpty(hoscode)) {
+            wrapper.eq("hoscode", hospitalSetQueryVo.getHoscode());
+        }
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, wrapper);
+        return Result.ok(pageHospitalSet);
+    }
+
+    @PostMapping("saveHospSet")
+    public Result saveHospSet(@RequestBody HospitalSet hospitalSet) {
+        hospitalSet.setStatus(1);
+        Random random = new Random();
+        hospitalSet.setSignKey(MD5.encrypt(System.currentTimeMillis() + "" + random.nextInt(1000)));
+        boolean save = hospitalSetService.save(hospitalSet);
+
+        if (save) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @GetMapping("getHospSet/{id}")
+    public Result getHospSet(@PathVariable Long id) {
+//        try {
+//            int a = 1 / 0;
+//        } catch (Exception e) {
+//            throw new YyghException("失败", 201);
+//        }
+//        int a = 1 / 0;
+
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok(hospitalSet);
+    }
+
+    @PostMapping("updateHospSet")
+    public Result updateHospSet(@RequestBody HospitalSet hospitalSet) {
+        boolean flag = hospitalSetService.updateById(hospitalSet);
+        if (flag) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
+    }
+
+    @DeleteMapping("batchRemoveHospSet")
+    public Result batchRemoveHospSet(@RequestBody List<Long> idList) {
+        hospitalSetService.removeByIds(idList);
+        return Result.ok();
+    }
+
+    @PutMapping("lockHospitalSet/{id}/{status}")
+    public Result lockHospitalSet(@PathVariable Long id, @PathVariable Integer status) {
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        hospitalSet.setStatus(status);
+        hospitalSetService.updateById(hospitalSet);
+        return Result.ok();
+    }
+
+    @PutMapping("sendKey/{id}")
+    public Result sendKey(@PathVariable Long id) {
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        String signKey = hospitalSet.getSignKey();
+        String hoscode = hospitalSet.getHoscode();
+
+        return Result.ok();
 
 
     }
+
 }
