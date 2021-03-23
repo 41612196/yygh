da390d8fd6a6d271eb6e83a8d2508aa11e0b0baf
41612196 <1185391863@qq.com>
2021-03-23 17:15:45 +0800

    医院管理

---
+++ hospital-manage/src/main/java/com/snnu/hospital_manage/HospitalManegeApplication.java
@@ -0,0 +1,13 @@
+package com.snnu.hospital_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManegeApplication.class, args);
+	}
+
+}
---
+++ hospital-manage/src/test/java/com/snnu/hospital_manage/HospitalManegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snnu.hospital_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
