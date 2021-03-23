98ae73f59414fd65e9917f3c269cf79eb7a7e141
41612196 <1185391863@qq.com>
2021-03-23 16:53:10 +0800

    医院管理

--- service/service_cmn/src/main/java/com/snnu/yygh/cmn/service/impl/DictServiceImpl.java
+++ service/service_cmn/src/main/java/com/snnu/yygh/cmn/service/impl/DictServiceImpl.java
@@ -10,6 +10,8 @@ import com.snnu.yygh.model.cmn.Dict;
 import com.snnu.yygh.vo.cmn.DictEeVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,7 +28,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
 
     //根据数据id查询子数据列表
     @Override
-    //@Cacheable(value = "dict",keyGenerator = "keyGenerator")
+    @Cacheable(value = "dict",keyGenerator = "keyGenerator")
     public List<Dict> findChlidData(Long id) {
         QueryWrapper<Dict> wrapper = new QueryWrapper<>();
         wrapper.eq("parent_id", id);
@@ -65,6 +67,7 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
     }
 
     @Override
+    @CacheEvict(value = "Dict", allEntries = true)
     public void importDictData(MultipartFile file) {
         try {
             EasyExcel.read(file.getInputStream(), DictEeVo.class, new DictListener(baseMapper)).sheet().doRead();
